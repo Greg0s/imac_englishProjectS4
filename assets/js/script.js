8404c0340a53773dd09df2a830d8e06ea5e8bd74
Greg0s <gregoire.tinnes@outlook.fr>
2023-03-30 23:56:34 +0200

    add clues + fullscreen()

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 var clueShowed = false;
+var isFullscreen = false;
 
 function show(selector){
     document.querySelectorAll(selector).forEach((element) => {
@@ -12,25 +13,75 @@ function hide(selector){
     })
 }
 
+function openFullscreen(){
+    let page = document.documentElement;
+    if (page.requestFullscreen) {
+        page.requestFullscreen();
+    } else if (page.webkitRequestFullscreen) { /* Safari */
+        page.webkitRequestFullscreen();
+    } else if (page.msRequestFullscreen) { /* IE11 */
+        page.msRequestFullscreen();
+    }
+    isFullscreen = true;
+}
+
+function closeFullscreen(){
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) { /* Safari */
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE11 */
+        document.msExitFullscreen();
+    }
+    isFullscreen = false;
+}
+
+function fullscreen(){
+    if(!isFullscreen){
+        openFullscreen();
+    }else{
+        closeFullscreen();
+    }
+
+}
+
 function closeBtn(){
     if(clueShowed){
         show('#close');
         document.querySelector('#close').onclick = function (){
             hide('.clues');
             hide('#close');
+            clueShowed = false;
         }
     }
 }
 
 function onClickElement(clickElement, showElement){
     document.querySelector(clickElement).onclick = function (){
-        show(showElement);
-        clueShowed = true;
-        closeBtn();
+        if(!clueShowed){
+            show(showElement);
+            clueShowed = true;
+            closeBtn();
+        }
     }
 }
 
 window.onload = function() {
-    onClickElement('#droite', '#testelt');
-    console.log(clueShowed);
+
+    //onClickElement('#drawer-d', '#diary');
+    onClickElement('#mud', '#mud-clue');
+    onClickElement('#wine1', '#wine1-clue');
+    onClickElement('#wine2', '#wine2-clue');
+    onClickElement('#hair', '#hair-clue');
+    onClickElement('#drawer-l-t', '#hair-clue');
+    onClickElement('#drawer-l-b', '#hair-clue');
+    onClickElement('#drawer-r', '#diary-clue');
+    onClickElement('#horsehair', '#hair-clue');
+    onClickElement('#portrait', '#portrait-clue');
+    onClickElement('#vase', '#vase-clue');
+    onClickElement('#books', '#books-clue');
+    onClickElement('#library', '#library-clue');
+    onClickElement('#horsehair', '#horsehair-clue');
+
+    //console.log(clueShowed);
   };
